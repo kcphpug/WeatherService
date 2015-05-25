4e564c63e1cf60e2a76dcd5c774f2ac648c54ab4
Daniel Holmes <daniel.holmes@gmail.com>
2015-05-25 11:30:04 -0500

    Basic passthough caching request functioning

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,18 +11,27 @@
 |
 */
 
-$app->get('/', function() use ($app) {
+$app->get( '/', function () use ( $app ) {
     return $app->welcome();
-});
+} );
 
-$app->get('{path:.*}', function($path) use ($app) {
-    $headersToPass = ['Content-Type','X-Pagination'];
-    $rootUrl = 'http://api.openweathermap.org/data/2.5/';
+$app->get( '{path:.*}', function ( $path ) use ( $app ) {
+    // Configure
+    $cacheTtl      = 60;
+    $headersToPass = [ 'Content-Type', 'X-Pagination' ];
+    $rootUrl       = 'http://api.openweathermap.org/data/2.5/';
+
     $queryString = $_SERVER['QUERY_STRING'];
+    $result      = Cache::remember(
+        $path . $queryString,
+        $cacheTtl,
+        function () use ( $path, $queryString, $app, $rootUrl ) {
+            $passThrough = $app->make( 'App\Http\Services\PassThrough', [ $rootUrl ] );
 
-    $passThrough = $app->make('App\PassThrough', [$rootUrl]);
+            return $passThrough->getResultsForPath( $path, $queryString );
+        }
 
-    $result = $passThrough->getResultsForPath($path, $queryString);
+    );
 
     return response(
         $result['body'],
@@ -32,6 +41,4 @@ $app->get('{path:.*}', function($path) use ($app) {
             $headersToPass
         )
     );
-});
-
-
+} );
